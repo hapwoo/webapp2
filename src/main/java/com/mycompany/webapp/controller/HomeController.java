99aec8a31934233391685ba1d2cd3fcf52a4f6ad
hapwoo <hapwoo@naver.com>
2021-01-13 18:37:08 +0900

    주석처리

--- src/main/java/com/mycompany/webapp/controller/HomeController.java
+++ src/main/java/com/mycompany/webapp/controller/HomeController.java
@@ -5,14 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+//
 @Controller
 public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping("/")
 	public String home() {
-		logger.info("실행");
+		logger.info("�떎�뻾");
 		return "home";
 	}
 
